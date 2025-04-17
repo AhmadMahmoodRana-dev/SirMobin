5c3533447f4d58ef9b0e92b04b4a655ce4e47bd3
AhmadMahmoodRana <techexpert10885@gmail.com>
2025-04-17 17:22:06 +0500

    second Services

--- src/pages/services/IntegrationServices.jsx
+++ src/pages/services/IntegrationServices.jsx
@@ -1,25 +1,12 @@
 export default function IntegrationServices() {
   const releatedServices = [
+    
     {
       "id": 1,
-      title:"Web Development",
-      link:""
+      title:"Advanced Infra Setup",
+      link:"infra-setup"
     },
-    {
-      "id": 2,
-      title:"Web Development",
-      link:""
-    },
-    {
-      "id": 3,
-      title:"Web Development",
-      link:""
-    },
-    {
-      "id": 4,
-      title:"Web Development",
-      link:""
-    },
+    
   ]
   return (
     <div className="flex flex-col lg:flex-row pt-32 bg-gray-900 text-white min-h-screen px-4 py-8 space-y-8 lg:space-y-0 lg:space-x-8">
@@ -52,14 +39,15 @@ export default function IntegrationServices() {
           <h3 className="text-xl font-bold mb-4 text-green-400">Related Services</h3>
           <ul className="space-y-3">
             {releatedServices.map((service, i) => (
-              <li
+              <a
+              href={service.link}
               
                 key={i}
                 className="group flex items-center p-3 rounded-lg hover:bg-gray-700 transition-all cursor-pointer"
               >
-                <a href={service.link} className="w-2 h-2 bg-green-400 rounded-full mr-3 opacity-0 group-hover:opacity-100 transition-all"></a>
+                <a  className="w-2 h-2 bg-green-400 rounded-full mr-3 opacity-0 group-hover:opacity-100 transition-all"></a>
                 {service.title}
-              </li>
+              </a>
             ))}
           </ul>
         </div>
