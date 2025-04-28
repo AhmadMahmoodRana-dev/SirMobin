230c664a6708987cfc1e0942b2e49edf75c75bba
mobin-ahmad <techexpert10885@gmail.com>
2025-04-28 15:37:19 +0500

    Update the scroll Issue

--- src/components/Home/ScrollToTop.jsx
+++ src/components/Home/ScrollToTop.jsx
@@ -2,11 +2,14 @@ import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 export default function ScrollToTop() {
-  const { pathname } = useLocation();
+  const { pathname, hash } = useLocation();
 
   useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
+    // If there's a hash (anchor link), don't scroll to top
+    if (!hash) {
+      window.scrollTo(0, 0);
+    }
+  }, [pathname, hash]);
 
   return null;
 }
